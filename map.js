1c3e96b58e33e394551abf3cf3b3353049c762b4
unknown <karmbirchima@gmail.com>
2018-06-26 22:00:38 -0700

    added code for hashtable

--- map.js
+++ map.js
@@ -3,23 +3,74 @@ class HashTable{
         this.hash = function(str, size){
             let hash = 0;
             for(let i = 0; i < str.length; i++){
-                hash += charCodeAt(str[i]);
+                hash += str.charCodeAt(i);
             }
             return hash % size;
         };
 
         this.storage = [];
+        this.bucketCount = 10;
     }
 
-    insert(){
+    insert(key, value){
+        let hashedKey = this.hash(key, this.bucketCount);
+        const storage = this.storage;
 
+        if(storage[hashedKey] === undefined){
+            storage[hashedKey] = [
+                [key, value]
+            ];
+        } else {
+            let inserted = false;
+
+            for(let i = 0; i < storage[hashedKey].length; i++){
+                if(storage[hashedKey][i][0] == key){
+                    storage[hashedKey][i][1] = value;
+                    inserted = true;
+                }
+            }
+            if(!inserted){
+                storage[hashedKey].push([key, value]);
+            }
+        }
     }
 
-    delete(){
+    delete(key){
+        let hashedKey = this.hash(key, this.bucketCount);
+        const storage = this.storage;
 
+        if(storage[hashedKey].length == 1 && storage[hashedKey][0][0] == key){
+            delete storage[hashedKey];
+        } else {
+            for(let i = 0; i < storage[hashedKey].length; i++){
+                if(storage[hashedKey][i][0] == key){
+                    delete storage[hashedKey][i];
+                }
+            }
+        }
     }
 
-    lookup(){
-        
+    lookup(key){
+        let hashedKey = this.hash(key, this.bucketCount);
+        const storage = this.storage;
+
+        if(storage[hashedKey].length == 1 && storage[hashedKey][0][0] == key){
+            return storage[hashedKey][0][1];
+        } else {
+            for(let i = 0; i < storage[hashedKey].length; i++){
+                if(storage[hashedKey][i][0] == key){
+                    return storage[hashedKey][i][1];
+                }
+            }
+            return undefined;
+        }
     }
 }
+
+let ht = new HashTable();
+ht.insert('beau', 'person');
+ht.insert('fido', 'dog');
+ht.insert('rex', 'dinosour');
+ht.insert('tux', 'penguin')
+console.log(ht.lookup('tux'))
+console.log(ht.storage);
